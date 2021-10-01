b04400816cd214a1a2149477dd0a0436380a94f9
Nessundorma <vismdbs@gmail.com>
2021-10-01 18:49:01 +0530

    Updated Setup.py

--- setup.py
+++ setup.py
@@ -2,22 +2,22 @@ import setuptools
 
 setuptools.setup(
     name="dictionarysnek",
-    version="0.1.0",
+    version="0.1.1",
     author="Example Author",
     author_email="author@example.com",
     description="A Dictionary API Wrapper for Python",
     long_description="A Module for those who are lazy as f$#% to find a Dictionary API and extract the data from it. Has numerous functions such as:- getsyn, getant, getdata, getdef and more...",
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/Gizems-Cult/dictionarysnek",
     project_urls={
-        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+        "Bug Tracker": "https://github.com/Gizems-Cult/dictionarysnek/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.6",
+    package_dir={"": "dictionarysnek"},
+    packages=setuptools.find_packages(where="dictionarysnek"),
+    python_requires=">=3.8",
 )
